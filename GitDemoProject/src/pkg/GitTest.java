5e016b84f66f449200bda38543dd368b7a00c1e0
Fredrick Lobo <getfred_07@yahoo.com>
2016-06-26 20:59:35 -0700

    Initial Commit

--- GitDemoProject/src/pkg/GitTest.java
+++ GitDemoProject/src/pkg/GitTest.java
@@ -1,12 +1,11 @@
 package pkg;
 
-public class TestingGit {
+public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello World");
 	}
-	
 
 }
